0e20d1e1ad0d42eef7b1bd53aabf9f4ac222d215
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-06-22 10:22:49 +0400

    add some changes for route

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ $router->group(['prefix' => 'api'], function () use ($router) {
         $router->group(['prefix' => 'project'], function () use ($router) {
             $router->get('/', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@index']);
             $router->post('/', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@store']);
-            $router->post('/edit', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@update']);
+            $router->post('/edit/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@update']);
             $router->get('/tasks', 'Project\ProjectController@tasks');
-            $router->post('/destroy/{id}', 'Project\ProjectController@destroy');
+            $router->post('/destroy/{id:[0-9]+}', 'Project\ProjectController@destroy');
         });
 
         $router->group(['prefix' => 'tasks'], function () use ($router) {
             $router->get('/', 'Task\TaskController@index');
             $router->post('/', ['middleware' => 'auth', 'uses' => 'Task\TaskController@store']);
-            $router->post('/edit', ['middleware' => 'auth', 'uses' => 'Task\TaskController@update']);
-            $router->post('/destroy/{id}', ['middleware' => 'auth', 'uses' => 'Task\TaskController@destroy']);
+            $router->post('/edit/{id:[0-9]+}', ['middleware' => 'auth', 'uses' => 'Task\TaskController@update']); //bug
+            $router->post('/destroy/{id:[0-9]+}', ['middleware' => 'auth', 'uses' => 'Task\TaskController@destroy']);
         });
 
         $router->group(['prefix' => 'user'], function () use ($router) {
@@ -37,7 +37,7 @@ $router->group(['prefix' => 'api'], function () use ($router) {
             $router->get('/profile', 'UserController@profile');
             $router->get('/projects', 'UserController@projects');
             $router->get('/tasks', 'UserController@tasks');
-            $router->get('/project/{id}/tasks', 'UserController@projectTasks');
+            $router->get('/project/{id:[0-9]+}/tasks', 'UserController@projectTasks');
         });
         $router->post('/login', 'AuthController@login');
     });
