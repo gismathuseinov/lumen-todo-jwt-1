3634628323ce84f3fd558136aa82206f39de9327
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-14 10:55:57 +0400

    add some routes

--- routes/web.php
+++ routes/web.php
@@ -16,30 +16,46 @@ $router->group(['prefix' => 'api'], function () use ($router) {
 
     $router->group(['prefix' => 'v1'], function () use ($router) {
 
+        // $router->group(['middleware' => 'cors'], function () use ($router) {
+
         $router->group(['prefix' => 'project'], function () use ($router) {
             $router->get('/', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@index']);
             $router->post('/', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@store']);
             $router->post('/edit/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@update']);
             $router->get('/tasks', 'Project\ProjectController@tasks');
             $router->post('/destroy/{id:[0-9]+}', 'Project\ProjectController@destroy');
+            $router->post('/add-user/', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@addUser']);
+            $router->get('/all-users/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Project\ProjectController@allUser']);
         });
 
         $router->group(['prefix' => 'tasks'], function () use ($router) {
             $router->get('/', 'Task\TaskController@index');
             $router->post('/', ['middleware' => 'auth', 'uses' => 'Task\TaskController@store']);
-            $router->post('/edit/{id:[0-9]+}', ['middleware' => 'auth', 'uses' => 'Task\TaskController@update']); //bug
-            $router->post('/destroy/{id:[0-9]+}', ['middleware' => 'auth', 'uses' => 'Task\TaskController@destroy']);
+            $router->post('/edit/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Task\TaskController@update']);
+            $router->post('/destroy/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Task\TaskController@destroy']);
+            $router->post('/add-user/', ['middleware' => 'manager', 'uses' => 'Task\TaskController@addUser']);
+            $router->get('/current-users/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'Task\TaskController@currentUsers']);
         });
 
         $router->group(['prefix' => 'user'], function () use ($router) {
             $router->get('/', ['middleware' => 'manager', 'uses' => 'UserController@index']);
             $router->post('/create', ['middleware' => 'manager', 'uses' => 'AuthController@store']);
             $router->get('/profile', 'UserController@profile');
+            $router->get('/project/{id:[0-9]+}/tasks', 'UserController@projectTasks');
             $router->get('/projects', 'UserController@projects');
             $router->get('/tasks', 'UserController@tasks');
-            $router->get('/project/{id:[0-9]+}/tasks', 'UserController@projectTasks');
+            $router->get('/logout', 'UserController@logout');
+            $router->post('/destroy/{id}', ['middleware' => 'manager', 'uses' => 'UserController@destroy']);
+            $router->post('/edit/{id}', ['middleware' => 'manager', 'uses' => 'UserController@edit']);
+            $router->post('/password-reset/{id}', ['middleware' => 'admin', 'uses' => 'AuthController@passwordReset']);
         });
+        $router->get('/total', ['middleware' => 'manager', 'uses' => 'UserController@total']);
+        $router->get('/users-list/{id:[0-9]+}', ['middleware' => 'manager', 'uses' => 'UserController@usersList']);
+
         $router->post('/login', 'AuthController@login');
+//        $router->get('/', function () use ($router) {
+//            return "Direct Access not permitted!";
+//        });
     });
 });
 
