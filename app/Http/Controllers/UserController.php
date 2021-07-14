b14f30887bad8ddafbf8db48e95c5a2d89300e53
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-14 10:55:18 +0400

    ad total,edit,destroy methods and modify projects method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,10 +7,13 @@ namespace App\Http\Controllers;
 use App\Models\Project;
 use App\Models\Task;
 use App\Models\User;
+use App\Models\UserProjects;
 use App\Models\UserTasks;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
@@ -30,7 +33,7 @@ class UserController extends Controller
                 $user->where('roles', $request->get('role'))->get();
             }
 
-            return response()->json($user->get());
+            return response()->json($user->where('id', '!=', Auth::id())->get());
         } else {
             return response()->json(User::findOrFail($request->id));
         }
@@ -38,9 +41,7 @@ class UserController extends Controller
 
     public function profile(): JsonResponse
     {
-        return response()->json([
-            'user' => Auth::user()
-        ]);
+        return response()->json(Auth::user());
     }
 
     public function logout(): JsonResponse
@@ -54,10 +55,27 @@ class UserController extends Controller
     public function projects(Request $request): JsonResponse
     {
         $userId = Auth::id();
-        $project = $request->has('id')
-            ? Project::where('user_id', $userId)->where('id', $request->id)->get()
-            : Auth::user()->projects;
-        return response()->json($project);
+        if($request->has('id')){
+            $project = Project::where('user_id', $userId)->where('id', $request->id)->get();
+            return response()->json($project);
+        }else{
+            $projectList = UserProjects::where('user_id',$userId)->get();
+
+            if(sizeof($projectList)!==0){
+                $projects = [];
+                foreach ($projectList as $item) {
+
+                    $data = Project::find($item['project_id']);
+                    $projects[] = $data;
+                }
+                if(sizeof($projects)===0){
+                    return response()->json();
+                }
+            }
+            return response()->json($projects);
+        }
+
+
     }
 
     public function tasks(Request $request): JsonResponse
@@ -68,7 +86,7 @@ class UserController extends Controller
                 $tasks->where('title', 'like', '%' . $request->get('title') . '%');
             }
             if ($request->has('close')) {
-                $tasks->where('close', 'like','%'.$request->get('close').'%');
+                $tasks->where('close', 'like', '%' . $request->get('close') . '%');
             }
             if ($request->has('status')) {
                 $tasks->where('status', $request->get('status'));
@@ -81,6 +99,7 @@ class UserController extends Controller
 
     public function projectTasks(int $id): JsonResponse
     {
+        $return = [];
         $tasks = UserTasks::select('task_id', 'project_id')
             ->where('user_id', Auth::id())
             ->where('project_id', $id)
@@ -91,7 +110,77 @@ class UserController extends Controller
             }
             return response()->json($return);
         } else {
-            return response()->json(['msg' => 'not found'], 404);
+            return response()->json($return, 200);
         }
+    }
+
+    public function destroy(int $id): JsonResponse
+    {
+        $user = User::find($id);
+        if ($user) {
+            $user->delete();
+            return response()->json([
+                'msg' => 'user deleted'
+            ], 200);
+        } else {
+            return response()->json(['msg' => 'User not found'], 404);
+        }
+    }
+
+    public function edit(Request $request, int $id): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'email' => ['required', 'email', 'unique:users'],
+            'name' => ['required', 'min:6', 'string'],
+            'roles' => ['required']
+        ]);
+        if ($validator->failed()) {
+            return response()->json([$validator->errors()], 404);
+        } else {
+            $user = DB::table('users')->where('id', $id)->update($request->all());
+            return response()->json($user, 200);
+        }
+    }
+
+    public function total(): JsonResponse
+    {
+        $user = User::count();
+        $doneTask = Task::where('status', \Status::DONE)->count();
+        $doingTask = Task::where('status', \Status::DOING)->count();
+        $projects = Project::count();
+        return response()->json([
+            'user' => $user,
+            'doneTask' => $doneTask,
+            'doingTask' => $doingTask,
+            'projects' => $projects
+        ], 200);
+    }
+
+    public function usersList(int $id): JsonResponse
+    {
+        $users = User::select('id', 'name', 'roles')->get();
+        foreach ($users as $user) {
+            $check = UserProjects::where('project_id', $id)->where('user_id', $user['id'])->count();
+            if ($check===0) {
+                $emptyUsers [] = $user;
+            }
+        }
+        $managers = [];
+        $employers = [];
+        if (!empty($emptyUsers)) {
+            foreach ($emptyUsers as $user) {
+                if ($user['roles'] == \Roles::MANAGER) {
+                    $managers[] = $user;
+                }
+                if ($user['roles'] === \Roles::EMPLOYER) {
+                    $employers[] = $user;
+                }
+            }
+            return response()->json([$managers, $employers], 200);
+
+        } else {
+            return response()->json(['err' => 'users not found'], 404);
+        }
+
     }
 }
