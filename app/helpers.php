561062369b2efc94e07963f1319fa6bf485f2888
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-14 10:51:46 +0400

    add Staic Status class in helpers

--- app/helpers.php
+++ app/helpers.php
@@ -1,8 +1,17 @@
 <?php
 
-class Roles{
-    const ADMIN = 0;
-    const MANAGER = 1;
-    const EMPLOYER = 2;
-    const USER = 4;
+class Roles
+{
+    const ADMIN = 1;
+    const MANAGER = 2;
+    const EMPLOYER = 3;
 }
+
+class Status
+{
+    const TODO = 1;
+    const DOING = 2;
+    const TEST = 3;
+    const DONE = 4;
+}
+
