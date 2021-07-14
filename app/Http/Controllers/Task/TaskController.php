35d3fa02a87dde7b643d6a3027905caa6e17fbde
Qismat Huseynov <50789493+q1sm4t@users.noreply.github.com>
2021-07-14 10:53:45 +0400

    add Adduser method

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Task;
 use App\Http\Controllers\Controller;
 use App\Models\Project;
 use App\Models\Task;
+use App\Models\User;
 use App\Models\UserTasks;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
@@ -40,18 +41,18 @@ class TaskController extends Controller
             'title' => ['required', 'string', 'min:5'],
             'descr' => ['required', 'string', 'min:20'],
             'close' => ['required'],
-            'status' => ['required', 'integer']
+            'status' => ['integer']
         ]);
         $request['project_id'] = (int)$request['project_id'];
 
         if ($validator->fails()) {
-            return response()->json($validator->errors());
+            return response()->json($validator->errors(), 404);
         } else {
             $projectCheck = Project::find($request['project_id']);
             if (!$projectCheck) {
                 return response()->json([
                     'err' => 'project not found'
-                ]);
+                ], 404);
             }
             $task = Task::create($request->all());
             $rel = UserTasks::create([
@@ -62,7 +63,7 @@ class TaskController extends Controller
             return response()->json([
                 'task' => $task,
                 'relation' => $rel
-            ]);
+            ], 200);
         }
     }
 
@@ -86,9 +87,8 @@ class TaskController extends Controller
                     'err' => 'project not found'
                 ]);
             }
-            $task = Task::findOrFail($id);
-            $task->update($request->all());
-            return response()->json($task);
+            $task = Db::table('tasks')->where('project_id', $request['project_id'])->where('id', $id)->update($request->all());
+            return response()->json(Task::find($id));
         }
     }
 
@@ -100,4 +100,51 @@ class TaskController extends Controller
 
     }
 
+    public function addUser(Request $request): JsonResponse
+    {
+        $validator = Validator::make($request->all(), [
+            'project_id' => ['required', 'integer'],
+            'user_id' => ['required', 'string'],
+            'task_id' => ['required', 'integer']
+        ]);
+        if ($validator->fails()) {
+            return response()->json($validator->errors());
+        } else {
+            $project = Project::find($request['project_id']);
+            if(!$project){
+                return response()->json(['err'=>'project not found'],404);
+            }
+            $user = User::find($request['user_id']);
+            if(!$user){
+                return response()->json(['err'=>'user not found'],404);
+            }
+            $task = Task::find($request['task_id']);
+            if(!$task){
+                return response()->json(['err'=>'Task not found'],404);
+            }
+            foreach ($request['user_id'] as $item) {
+                $ret = UserTasks::create([
+                    'user_id' => $item,
+                    'project_id' => $request['project_id'],
+                    'task_id'  => $request['task_id']
+                ]);
+            }
+            return response()->json(['msg'=>'added']);
+
+        }
+
+    }
+
+    public function currentUsers(int $id): JsonResponse
+    {
+        $userIds = UserTasks::select('user_id')->where('task_id',$id)->get();
+        foreach ($userIds as $userId) {
+            $user = User::select('id','name')->where('id',$userId['user_id'])->get();
+            $users[] = $user;
+        }
+        return response()->json($users);
+    }
+
 }
+
+
